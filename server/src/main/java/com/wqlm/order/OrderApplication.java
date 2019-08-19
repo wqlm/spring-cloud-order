537b514e72e86fc84bb57de6deba292485c2ac1f
wqlm <649924325@qq.com>
2019-08-19 18:43:00 +0800

    10-6 feign-hystrix的使用

--- server/src/main/java/com/wqlm/order/OrderApplication.java
+++ server/src/main/java/com/wqlm/order/OrderApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 //@SpringBootApplication
 //@EnableDiscoveryClient
@@ -13,6 +15,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 // @SpringCloudApplication 包含上面三个注解
 @SpringCloudApplication
 @EnableFeignClients(basePackages = "com.wqlm.product.client")
+@ComponentScan(basePackages = "com.wqlm")
 public class OrderApplication {
 
 	public static void main(String[] args) {
